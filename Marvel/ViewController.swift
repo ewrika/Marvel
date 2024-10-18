35148a760568faec0aa173443dfe17f464b6f1e9
ewrika <egorkaomsk_2003@mail.ru>
2024-10-18 21:43:55 +0600

    Initial Commit

---
+++ Marvel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel
+//
+//  Created by Георгий Борисов on 18.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
