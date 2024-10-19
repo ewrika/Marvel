5c80820f0f71bb5d6f52c7582cebf47e3442049e
ewrika <egorkaomsk_2003@mail.ru>
2024-10-20 02:01:22 +0600

    feature: Add logo in View

--- Marvel/ViewController.swift
+++ Marvel/ViewController.swift
@@ -1,23 +1,37 @@
-//
-//  ViewController.swift
-//  Marvel
-//
-//  Created by Георгий Борисов on 18.10.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let logoMarvel: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "marvelLogo")
+        return imageView
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //check for work without storyboards
-        view.backgroundColor = .red
-        
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(named: "Background")
+        SetupAutoLayout()
+        MarvelLogo()
     }
-
+    
+    private func SetupAutoLayout(){
+        
+        view.addSubview(logoMarvel)
+        
+    }
+    
+    private func MarvelLogo() {
+        NSLayoutConstraint.activate([
+            logoMarvel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            logoMarvel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            logoMarvel.heightAnchor.constraint(equalToConstant: 27),
+            logoMarvel.widthAnchor.constraint(equalToConstant: 128)
+        ])
+    }
 }
 
 #Preview {
